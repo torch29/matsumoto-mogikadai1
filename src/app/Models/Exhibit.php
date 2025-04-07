5932be2fa9330af585918d5531915a5bded3101d
Torch29 <ashtonerico@yahoo.co.jp>
2025-04-07 19:59:32 +0900

    モデルにリレーション設定を記述

--- src/app/Models/Exhibit.php
+++ src/app/Models/Exhibit.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Exhibit extends Model
 {
     use HasFactory;
+
+    public function condition()
+    {
+        return $this->belongsTo(Condition::class);
+    }
+
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function exhibitedItems()
+    {
+        return $this->belongsToMany(Exhibit::class)->withTimestamps();
+    }
 }

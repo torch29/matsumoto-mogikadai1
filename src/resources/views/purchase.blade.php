e612715f1932b581629c940a51e02f2ad0a82953
Torch29 <ashtonerico@yahoo.co.jp>
2025-04-15 23:12:48 +0900

    purchase.blade.phpの基本的な配置を作成

--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -5,14 +5,16 @@
 @endsection
 
 @section('content')
-<div class="purchase-content">
-    <div class="purchase-content__head">
-        <div class="purchase-content__head--img">
-            <img src="" alt="商品画像">
-        </div>
-        <div class="purchase-content__head--info">
-            <span>商品名</span>
-            <p>￥ 金額表示</p>
+<div class="purchase-content__wrapper">
+    <div class="purchase-content">
+        <div class="purchase-content__head">
+            <div class="purchase-content__head--img">
+                <img src="" alt="商品画像">
+            </div>
+            <div class="purchase-content__head--info">
+                <span>商品名</span>
+                <p>￥ 金額表示</p>
+            </div>
         </div>
         <div class="purchase-content__payment">
             <p class="purchase-content__label">
@@ -24,20 +26,28 @@
             </select>
         </div>
         <div class="purchase-content__send">
-            <p class="purchase-content__label">配送先</p>
-            <a href="" class="purchase-content__link">変更する</a>
-            <p>〒</p>
-            <p>住所表示欄</p>
+            <div class="purchase-content__address">
+                <p class="purchase-content__address-label">配送先</p>
+                <p>〒</p>
+                <p>住所表示欄</p>
+            </div>
+            <div class="purchase-content__change-address">
+                <a href="" class="purchase-content__link">変更する</a>
+            </div>
         </div>
     </div>
     <div class="purchase-content__confirm">
         <div class="confirm__area">
             <form action="" class="confirm-form">
                 @csrf
-                <label for="" class="confirm-form__label">商品代金</label>
-                <input type="text" class="confirm-form__input" readonly value="￥ 47,000">
-                <label for="" class="confirm-form__label">支払い方法</label>
-                <input type="text" class="confirm-form__input" readonly value="コンビニ払い">
+                <div class="confirm-form__row">
+                    <p class="confirm-form__row-title">商品代金</p>
+                    <p class="confirm-form__row-item">￥ 47,000</p>
+                </div>
+                <div class="confirm-form__row">
+                    <p class="confirm-form__row-title">支払い方法</p>
+                    <p class="confirm-form__row-item">コンビニ払い</p>
+                </div>
                 <div class="confirm-form__button">
                     <button class="confirm-form__button-submit">購入する</button>
                 </div>

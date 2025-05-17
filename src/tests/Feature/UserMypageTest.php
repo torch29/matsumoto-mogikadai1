67fab34f35a1f4b1db149ae8b0e9067f06e8bc52
Torch29 <ashtonerico@yahoo.co.jp>
2025-05-18 01:27:54 +0900

    UserMypgeTestにテストを2件追加

--- src/tests/Feature/UserMypageTest.php
+++ src/tests/Feature/UserMypageTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\Item;
 use App\Models\Profile;
+use App\Models\Purchase;
 
 class UserMypageTest extends TestCase
 {
@@ -18,10 +19,94 @@ class UserMypageTest extends TestCase
      */
     use RefreshDatabase;
 
-    public function test_example()
+    public function test_show_users_information()
     {
-        $response = $this->get('/');
+        $user = User::factory()
+            ->has(Profile::factory([
+                'profile_img' => 'images/user/icon.png',
+            ]))
+            ->create([
+                'name' => 'テスト　ユーザー',
+            ]);
+        $this->actingAs($user);
 
-        $response->assertStatus(200);
+        //テスト　ユーザーが出品した商品を2件作成
+        $userSoldItem1 = Item::factory()->create([
+            'user_id' => $user->id,
+            'name' => 'テストさんが出品した商品',
+        ]);
+        $userSoldItem2 = Item::factory()->create([
+            'user_id' => $user->id,
+            'name' => 'テスト出品商品２'
+        ]);
+
+        //テスト　ユーザーが購入した商品を2件作成
+        $purchasedItem1 = Item::factory()->create([
+            'name' => 'テストさんが購入した商品',
+            'status' => 'sold'
+        ]);
+        Purchase::create([
+            'user_id' => $user->id,
+            'item_id' => $purchasedItem1->id,
+            'payment' => 'card',
+            'zip_code' => $user->profile->zip_code,
+            'address' => $user->profile->address,
+        ]);
+        $purchasedItem2 = Item::factory()->create([
+            'name' => 'テスト購入商品２',
+            'status' => 'pending'
+        ]);
+        Purchase::create([
+            'user_id' => $user->id,
+            'item_id' => $purchasedItem2->id,
+            'payment' => 'konbini',
+            'zip_code' => $user->profile->zip_code,
+            'address' => $user->profile->address,
+        ]);
+
+        //マイページにアクセスし、ユーザーアイコン、ユーザー名、出品した商品名、購入した商品名が表示されていることを確認
+        $response = $this->get('mypage');
+        $response->assertViewIs('user.mypage');
+
+        $response->assertSee('images/user/icon.png');
+        $response->assertSee('テスト　ユーザー');
+        $response->assertSeeInOrder([
+            'id="sellItems"',
+            'テストさんが出品した商品',
+            'テスト出品商品２'
+        ], false);
+        $response->assertSeeInOrder([
+            'id="purchasedItems"',
+            'テストさんが購入した商品',
+            'テスト購入商品２'
+        ], false);
+        //$response->dump();
+    }
+
+    public function test_show_users_initial_profile_data()
+    {
+        //ユーザー作成し、プロフィールの初期値を設定
+        $user = User::factory()
+            ->has(Profile::factory([
+                'profile_img' => 'images/user/icon.png',
+                'zip_code' => '000-1234',
+                'address' => '北海道札幌市123',
+                'building' => 'ダミーマンション'
+            ]))
+            ->create([
+                'name' => 'テスト　ユーザー',
+            ]);
+        $this->actingAs($user);
+
+        //プロフィール編集ページにアクセスし、初期値が表示されていることを確認
+        $response = $this->get('/mypage/profile');
+        $response->assertViewIs('user.profile');
+        $response->assertSeeInOrder([
+            'テスト　ユーザー',
+            '000-1234',
+            '北海道札幌市123',
+            'ダミーマンション'
+        ], false);
+        //$response->dump();
     }
 }

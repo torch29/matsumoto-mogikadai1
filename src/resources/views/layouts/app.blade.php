a29b2fb6185a40803fb00d8cdc9587405d7ebf2f
Torch29 <ashtonerico@yahoo.co.jp>
2025-05-17 23:21:01 +0900

    detail.blade.phpの修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,48 +12,6 @@
 </head>
 
 <body>
-    {{--
-    <header class="header">
-        <nav class="header-nav">
-            <ul class="header-nav__list">
-                <li class="header-nav__list-item">
-                    <a href="/">
-                        <img src="{{ asset('img/logo.svg') }}" class="header__title-img" alt="COACHTECHフリマ">
-    </a>
-    </li>
-    <li class="header-nav__list-item">
-        <form action="/" class="search-form" method="get">
-            @csrf
-            <input type="text" name="search" class="search-form__input" placeholder="なにをお探しですか？" value="{{ isset($word) ? $word : '' }}">
-            <button class="search-form__button--hidden">検索</button>
-        </form>
-    </li>
-    <li class="header-nav__list-item">
-        <ul class="header-nav__list--user">
-            @if (Auth::check())
-            <li class="header-nav__user-item">
-                <form action="/logout" class="header-nav__form" method="post">
-                    @csrf
-                    <button class="header-nav__button-submit">ログアウト</button>
-                </form>
-            </li>
-            @else
-            <li class="header-nav__user-item">
-                <form action="/login" method="get">
-                    @csrf
-                    <button class="header-nav__button-submit">ログイン</button>
-                </form>
-            </li>
-            @endif
-            <li class="header-nav__user-item"><a href="/mypage">マイページ</a></li>
-            <a href="/sell" class="header-nav__button">出品</a>
-        </ul>
-    </li>
-    </ul>
-    </nav>
-    </header>
-    --}}
-
     <header class="header">
         <nav class="header-nav">
             <h1 class="header-nav__ttl">

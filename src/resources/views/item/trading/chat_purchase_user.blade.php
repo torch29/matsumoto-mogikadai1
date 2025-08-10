451123e98908932938f11de9d90d3bd965f84e92
Torch29 <ashtonerico@yahoo.co.jp>
2025-08-10 22:55:56 +0900

    取引チャット画面のcss修正

--- src/resources/views/item/trading/chat_purchase_user.blade.php
+++ src/resources/views/item/trading/chat_purchase_user.blade.php
@@ -21,51 +21,60 @@
             @endforeach
         </div>
     </div>
-    <div class="chat__main">
-        <div class="chat__heading">
-            <div class="heading__title">
-                <div class="heading__info--user">
-                    <div class="heading__icon">
-                        <img src="" alt="">
-                    </div>
-                    <h2>{{ $tradingItem->purchasedItem->users->name }}さんとの取引画面</h2>
+    <div class="chat__heading">
+        <div class="heading__title">
+            <div class="heading__info--user">
+                <div class="heading__icon">
+                    <img src="" alt="">
                 </div>
-                <div class="heading__button">
-                    <button>
-                        取引を完了する
-                    </button>
-                </div>
+                <h2>{{ $tradingItem->purchasedItem->users->name }}さんとの取引画面</h2>
+            </div>
+            <div class="heading__button">
+                <button>
+                    取引を完了する
+                </button>
             </div>
         </div>
-        <div class="chat__info--area">
-            <div class="info__img">
-                <img src="{{ asset($tradingItem->purchasedItem->img_path) }}" alt="商品画像">
+    </div>
+    <div class="chat__info--area">
+        <div class="info__img">
+            <img src="{{ asset($tradingItem->purchasedItem->img_path) }}" alt="商品画像">
+        </div>
+        <div class="info__item--lead">
+            <div class="info__item--name">
+                {{ $tradingItem->purchasedItem->name }}
             </div>
-            <div class="info__item--lead">
-                <div class="info__item--name">
-                    {{ $tradingItem->purchasedItem->name }}
-                </div>
-                <div class="info__item--price">
-                    ￥{{ number_format($tradingItem->purchasedItem->price) }}
-                </div>
+            <div class="info__item--price">
+                ￥{{ number_format($tradingItem->purchasedItem->price) }}
             </div>
         </div>
-        <div class="chat__content">
-            {{ dump($tradingItem->purchasedItem->img_path) }}
-            <div class="message__block">
-                <div class="message-header">
-                    <div class="message-header__icon"></div>
-                    <div class="message-header__name">
-                        購入者（自分）：{{ $tradingItem->purchasedUser->name }}
-                        出品者（相手）：{{ $tradingItem->purchasedItem->users->name
+    </div>
+    <div class="chat__content">
+        {{ dump($tradingItem->purchasedItem->img_path) }}
+        <div class="message__block">
+            <div class="message-header">
+                <div class="message-header__icon"></div>
+                <div class="message-header__name">
+                    購入者（自分）：{{ $tradingItem->purchasedUser->name }}
+                    出品者（相手）：{{ $tradingItem->purchasedItem->users->name
                         }}
-                    </div>
-                </div>
-                <div class="message">
-                    メッセージが表示される
                 </div>
             </div>
+            <div class="message">
+                メッセージが表示される
+            </div>
         </div>
+    </div>
+    <div class="chat__footer">
+        <form action="/send" method="post" class="chat-form">
+            @csrf
+            <input type="text" class="chat__input" placeholder="取引メッセージを記入してください">
+            <div class="chat__actions">
+                <button>
+                    <img src="" alt="送信">
+                </button>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
